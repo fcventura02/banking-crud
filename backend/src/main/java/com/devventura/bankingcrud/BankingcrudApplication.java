59500d40ff867ad0c6d651a3775ca225fbc390f4
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2021-01-07 19:53:55 -0300

    Project Created

---
+++ backend/src/main/java/com/devventura/bankingcrud/BankingcrudApplication.java
@@ -0,0 +1,13 @@
+package com.devventura.bankingcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingcrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devventura/bankingcrud/BankingcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devventura.bankingcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
