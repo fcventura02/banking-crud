e060101f0278f608dfb1d8807c0ae6534a0ebe3a
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2021-01-10 01:05:57 -0300

    Created repositores

---
+++ backend/src/main/java/com/devventura/bankingcrud/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package com.devventura.bankingcrud.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devventura.bankingcrud.entities.People;
+
+public interface PeopleRepository extends JpaRepository<People, Long> {
+	People findOneByEmail(String email);
+	People findOneByCpf(String cpf);
+}
